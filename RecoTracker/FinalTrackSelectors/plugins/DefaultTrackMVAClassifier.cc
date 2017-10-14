c92bb1fcccd0b9f9925404640d3924f17cb90e26
hajohajo@helsinki.fi <hajohajo@helsinki.fi>
2017-10-14 12:18:23 +0200

    Working implementation

--- RecoTracker/FinalTrackSelectors/plugins/DefaultTrackMVAClassifier.cc
+++ RecoTracker/FinalTrackSelectors/plugins/DefaultTrackMVAClassifier.cc
@@ -27,23 +27,20 @@ struct dnn {
 
   {}
 
-  void beginStream(){}
-
-  void initEvent(const edm::EventSetup& es) {
-    path_ = "/afs/cern.ch/work/j/jhavukai/private/LWTNNinCMSSW/CMSSW_9_3_X_2017-09-25-1100/src/Tensorflow_graph";
-    metaGraph_ = tf::loadMetaGraph(path_);
-    session_ = tf::createSession(metaGraph_,path_);
-    x_ = tf::Tensor(tf::DT_FLOAT,{1,22});
-//    inputs_.push_back(x_);
+  void beginStream(){
+    path_ = "/afs/cern.ch/work/j/jhavukai/private/LWTNNinCMSSW/NewTest/CMSSW_9_4_X_2017-10-01-0000/src/Tensorflow_graph";
     input_names_.push_back("ins");
     output_names_.push_back("outs/Sigmoid");
+    metaGraph_ = tf::loadMetaGraph(path_);
+    session_ = tf::createSession(metaGraph_,path_);
   }
+
+  void initEvent(const edm::EventSetup& es) {}
 
   float operator()(reco::Track const & trk,
 		   reco::BeamSpot const & beamSpot,
 		   reco::VertexCollection const & vertices) const {
 
-    std::cout<<"Start of operator"<<std::endl;
     auto tmva_pt_ = trk.pt();
     auto tmva_eta_ = trk.eta();
     auto tmva_lambda_ = trk.lambda();
@@ -68,48 +65,41 @@ struct dnn {
     auto tmva_nLostLay_ = trk.hitPattern().trackerLayersWithoutMeasurement(reco::HitPattern::TRACK_HITS);
     auto tmva_algo_ = trk.algo();
 
-    std::vector<float> gbrVals_; 
-    gbrVals_.push_back(tmva_pt_);
-    gbrVals_.push_back(tmva_eta_);
-    gbrVals_.push_back(tmva_lambda_);
-    gbrVals_.push_back(tmva_absd0_);
-    gbrVals_.push_back(tmva_absdz_);
-    gbrVals_.push_back(tmva_absd0PV_);
-    gbrVals_.push_back(tmva_absdzPV_);
-    gbrVals_.push_back(tmva_ptErr_);
-    gbrVals_.push_back(tmva_etaErr_);
-    gbrVals_.push_back(tmva_lambdaErr_);
-    gbrVals_.push_back(tmva_dxyErr_);
-    gbrVals_.push_back(tmva_dzErr_);
-    gbrVals_.push_back(tmva_nChi2_);
-    gbrVals_.push_back(tmva_ndof_);
-    gbrVals_.push_back(tmva_nInvalid_);
-    gbrVals_.push_back(tmva_nPixel_);
-    gbrVals_.push_back(tmva_nStrip_);
-    gbrVals_.push_back(tmva_nPixelLay_);
-    gbrVals_.push_back(tmva_nStripLay_);
-    gbrVals_.push_back(tmva_n3DLay_);
-    gbrVals_.push_back(tmva_nLostLay_);
-    gbrVals_.push_back(tmva_algo_);
-
-    std::cout<<"Start of tensorflow magic"<<std::endl;
-    //This is silly, fixing it soon
+    std::vector<tf::Tensor> inputs_;
     tf::Tensor input(tf::DT_FLOAT,{1,22});
-//    for (size_t i = 0; i<22;i++) input.matrix<float>()(0,i) = float(i);
-    std::vector<tf::Tensor> innes_;
-    std::cout<<"Loading x_"<<std::endl;
-    for (size_t i = 0; i<22;i++) input.matrix<float>()(0,i) = gbrVals_[i];
-    innes_.push_back(input);
+    input.matrix<float>()(0,0)=tmva_pt_;
+    input.matrix<float>()(0,1)=tmva_eta_;
+    input.matrix<float>()(0,2)=tmva_lambda_;
+    input.matrix<float>()(0,3)=tmva_absd0_;
+    input.matrix<float>()(0,4)=tmva_absdz_;
+    input.matrix<float>()(0,5)=tmva_absd0PV_;
+    input.matrix<float>()(0,6)=tmva_absdzPV_;
+    input.matrix<float>()(0,7)=tmva_ptErr_;
+    input.matrix<float>()(0,8)=tmva_etaErr_;
+    input.matrix<float>()(0,9)=tmva_lambdaErr_;
+    input.matrix<float>()(0,10)=tmva_dxyErr_;
+    input.matrix<float>()(0,11)=tmva_dzErr_;
+    input.matrix<float>()(0,12)=tmva_nChi2_;
+    input.matrix<float>()(0,13)=tmva_ndof_;
+    input.matrix<float>()(0,14)=tmva_nInvalid_;
+    input.matrix<float>()(0,15)=tmva_nPixel_;
+    input.matrix<float>()(0,16)=tmva_nStrip_;
+    input.matrix<float>()(0,17)=tmva_nPixelLay_;
+    input.matrix<float>()(0,18)=tmva_nStripLay_;
+    input.matrix<float>()(0,19)=tmva_n3DLay_;
+    input.matrix<float>()(0,20)=tmva_nLostLay_;
+    input.matrix<float>()(0,21)=tmva_algo_;
+
+    inputs_.push_back(input);
 
     std::vector<tf::Tensor> outputs_;
 
-    std::cout<<"Running..."<<std::endl;
     tf::run(session_,input_names_,inputs_,output_names_, &outputs_);
 
-    std::cout<<"Reading output"<<std::endl;
     float dnn_value = outputs_[0].matrix<float>()(0,0);
-//    float dnn_value = 0.0;
-    std::cout<<dnn_value<<std::endl;
+
+    inputs_.clear();
+    outputs_.clear();
 
     return dnn_value;
   }
@@ -121,9 +111,6 @@ struct dnn {
   std::string path_;
   tf::Session* session_ = nullptr; //*
   tf::MetaGraphDef* metaGraph_ = nullptr; //*
-  tf::Tensor x_; // = nullptr;
-  tf::Tensor* y_ = nullptr;
-  std::vector<tf::Tensor> inputs_;
   std::vector<std::string> input_names_;
   std::vector<std::string> output_names_;
 
